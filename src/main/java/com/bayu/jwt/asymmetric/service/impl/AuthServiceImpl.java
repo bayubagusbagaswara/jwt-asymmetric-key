005a8af465dda68b4d3adf0a9f8781a1829a022d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-02 09:30:03 +0700

    add service impl AuthServiceImpl

---
+++ src/main/java/com/bayu/jwt/asymmetric/service/impl/AuthServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bayu.jwt.asymmetric.service.impl;
+
+import com.bayu.jwt.asymmetric.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthServiceImpl.class);
+
+}
