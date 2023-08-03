ab84e5fbd32dcc09a2ef20d4c34e266a219f5124
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-03 20:52:55 +0700

    add method signIn

--- src/main/java/com/bayu/jwt/asymmetric/service/AuthService.java
+++ src/main/java/com/bayu/jwt/asymmetric/service/AuthService.java
@@ -1,10 +1,14 @@
 package com.bayu.jwt.asymmetric.service;
 
+import com.bayu.jwt.asymmetric.dto.AuthenticationResponse;
+import com.bayu.jwt.asymmetric.dto.LoginRequest;
 import com.bayu.jwt.asymmetric.dto.RegistrationRequest;
 import com.bayu.jwt.asymmetric.dto.RegistrationResponse;
 
 public interface AuthService {
 
     RegistrationResponse registration(RegistrationRequest request);
+
+    AuthenticationResponse signIn(LoginRequest request);
 
 }
