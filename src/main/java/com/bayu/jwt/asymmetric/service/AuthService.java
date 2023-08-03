c5460798b2b8e1a0432e423b2f0e123a9a951e82
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-03 20:52:17 +0700

    add method registration

--- src/main/java/com/bayu/jwt/asymmetric/service/AuthService.java
+++ src/main/java/com/bayu/jwt/asymmetric/service/AuthService.java
@@ -1,4 +1,10 @@
 package com.bayu.jwt.asymmetric.service;
 
+import com.bayu.jwt.asymmetric.dto.RegistrationRequest;
+import com.bayu.jwt.asymmetric.dto.RegistrationResponse;
+
 public interface AuthService {
+
+    RegistrationResponse registration(RegistrationRequest request);
+
 }
