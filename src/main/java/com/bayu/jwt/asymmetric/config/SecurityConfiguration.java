e8742529dbf17c075332c231ca678792a939a115
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-03 21:15:03 +0700

    add annotation required args constructor

--- src/main/java/com/bayu/jwt/asymmetric/config/SecurityConfiguration.java
+++ src/main/java/com/bayu/jwt/asymmetric/config/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.bayu.jwt.asymmetric.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 }
