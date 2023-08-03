93fc67e261dcc969f2edaf2f7e8c4a4b5459d22c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-03 20:53:29 +0700

    add method registration and signIn

--- src/main/java/com/bayu/jwt/asymmetric/service/impl/AuthServiceImpl.java
+++ src/main/java/com/bayu/jwt/asymmetric/service/impl/AuthServiceImpl.java
@@ -1,5 +1,9 @@
 package com.bayu.jwt.asymmetric.service.impl;
 
+import com.bayu.jwt.asymmetric.dto.AuthenticationResponse;
+import com.bayu.jwt.asymmetric.dto.LoginRequest;
+import com.bayu.jwt.asymmetric.dto.RegistrationRequest;
+import com.bayu.jwt.asymmetric.dto.RegistrationResponse;
 import com.bayu.jwt.asymmetric.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -11,5 +15,15 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private static final Logger log = LoggerFactory.getLogger(AuthServiceImpl.class);
+
+    @Override
+    public RegistrationResponse registration(RegistrationRequest request) {
+        return null;
+    }
+
+    @Override
+    public AuthenticationResponse signIn(LoginRequest request) {
+        return null;
+    }
 
 }
