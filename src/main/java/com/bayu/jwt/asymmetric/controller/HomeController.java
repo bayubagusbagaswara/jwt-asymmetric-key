f8bf21594accd966f9fb82ec5e43464e4dbce20a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-03 20:50:41 +0700

    add method home

--- src/main/java/com/bayu/jwt/asymmetric/controller/HomeController.java
+++ src/main/java/com/bayu/jwt/asymmetric/controller/HomeController.java
@@ -1,9 +1,17 @@
 package com.bayu.jwt.asymmetric.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/home")
 public class HomeController {
+
+    @GetMapping
+    public String home(Authentication authentication) {
+        return "Hello ," + authentication.getName() + "token is valid";
+    }
+
 }
