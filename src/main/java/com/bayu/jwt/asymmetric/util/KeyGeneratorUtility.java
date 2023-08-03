37845f33046f1394212f1093db79e959c1397b6f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-03 20:59:42 +0700

    add method generateRsaKey

--- src/main/java/com/bayu/jwt/asymmetric/util/KeyGeneratorUtility.java
+++ src/main/java/com/bayu/jwt/asymmetric/util/KeyGeneratorUtility.java
@@ -2,7 +2,22 @@ package com.bayu.jwt.asymmetric.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
 @UtilityClass
 public class KeyGeneratorUtility {
+
+    public static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+        return keyPair;
+    }
 
 }
