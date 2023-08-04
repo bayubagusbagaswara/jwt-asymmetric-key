ed22f32c8a49d8e1d4c621ef7b1bc49edafc3e1c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-04 22:00:25 +0700

    inject AuthService

--- src/main/java/com/bayu/jwt/asymmetric/controller/AuthController.java
+++ src/main/java/com/bayu/jwt/asymmetric/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.bayu.jwt.asymmetric.controller;
 
+import com.bayu.jwt.asymmetric.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
 }
