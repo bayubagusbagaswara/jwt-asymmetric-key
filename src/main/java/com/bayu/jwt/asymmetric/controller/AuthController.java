dcb02fe7a74d1ccc9506750ba9a4323d38be04e7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-04 22:00:54 +0700

    add method registration

--- src/main/java/com/bayu/jwt/asymmetric/controller/AuthController.java
+++ src/main/java/com/bayu/jwt/asymmetric/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.bayu.jwt.asymmetric.controller;
 
+import com.bayu.jwt.asymmetric.dto.ApiResponse;
+import com.bayu.jwt.asymmetric.dto.RegistrationRequest;
+import com.bayu.jwt.asymmetric.dto.RegistrationResponse;
 import com.bayu.jwt.asymmetric.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping(path = "/registration")
+    public ResponseEntity<ApiResponse<RegistrationResponse>> registration(@RequestBody RegistrationRequest request) {
+        RegistrationResponse registrationResponse = authService.registration(request);
+        return ResponseEntity.ok()
+                .body(new ApiResponse<>(Boolean.TRUE, "Success", registrationResponse));
+    }
+
 }
