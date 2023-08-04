121eedac31cfd66df323841d3c7d89ccd7c2b40c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-04 22:03:21 +0700

    implement method loadUserByUsername

--- src/main/java/com/bayu/jwt/asymmetric/service/impl/UserServiceImpl.java
+++ src/main/java/com/bayu/jwt/asymmetric/service/impl/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.bayu.jwt.asymmetric.service.impl;
 
+import com.bayu.jwt.asymmetric.repository.UserRepository;
 import com.bayu.jwt.asymmetric.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +14,13 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findFirstByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username [" + username + "] not found"));
+    }
+
 }
