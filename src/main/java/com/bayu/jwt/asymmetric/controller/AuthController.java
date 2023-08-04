e3ebe14b55cc0d52e18214a750bf335c453cc0d4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-08-04 22:01:17 +0700

    add method signIn

--- src/main/java/com/bayu/jwt/asymmetric/controller/AuthController.java
+++ src/main/java/com/bayu/jwt/asymmetric/controller/AuthController.java
@@ -1,8 +1,6 @@
 package com.bayu.jwt.asymmetric.controller;
 
-import com.bayu.jwt.asymmetric.dto.ApiResponse;
-import com.bayu.jwt.asymmetric.dto.RegistrationRequest;
-import com.bayu.jwt.asymmetric.dto.RegistrationResponse;
+import com.bayu.jwt.asymmetric.dto.*;
 import com.bayu.jwt.asymmetric.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +21,13 @@ public class AuthController {
         RegistrationResponse registrationResponse = authService.registration(request);
         return ResponseEntity.ok()
                 .body(new ApiResponse<>(Boolean.TRUE, "Success", registrationResponse));
+    }
+
+    @PostMapping(path = "/signin")
+    public ResponseEntity<ApiResponse<AuthenticationResponse>> signIn(@RequestBody LoginRequest loginRequest) {
+        AuthenticationResponse authenticationResponse = authService.signIn(loginRequest);
+        return ResponseEntity.ok()
+                .body(new ApiResponse<>(Boolean.TRUE, "Success", authenticationResponse));
     }
 
 }
